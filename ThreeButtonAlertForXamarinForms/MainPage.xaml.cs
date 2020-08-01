453abd2248488dc3f83b28b6b3b1e22e117486c2
Damian Antonowicz <kontakt@damianantonowicz.pl>
2020-08-01 23:13:22 +0200

    Added default Xamarin.Forms app.

---
+++ ThreeButtonAlertForXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace ThreeButtonAlertForXamarinForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var alert = DependencyService.Get<IAlert>();
+            var result = await alert.Display("Title", "Message", "First Button", "Second button", "Cancel");
+
+            await DisplayAlert("", "clicked on: " + result, "OK");
+        }
+    }
+}
